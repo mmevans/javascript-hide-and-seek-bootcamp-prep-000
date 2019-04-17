343d9fda33ada7629b06fec077c7c7304ba586b1
Michael Evans <mmevansjr@gmail.com>
2019-04-17 19:55:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 
+var getFirstSelector = function(selector) {
+  return document.querySelector(selector);
+};
+
+var nestedTarget = function() {
+  return document.getElementById('nested').querySelector('.target');
+  
+};
+
+var increaseRankBy = function(n) {
+ var lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+ for (let i = 0; i < lis.length; i++) {
+   lis[i].innerHTML=parseInt(lis[i].innerHTML)+n;
+ }
+ return test;
+  
+};
+
+var deepestChild = function() {
+  var lis=document.getElementById("grand-node").querySelectorAll("div");
+  var test;
+  for(let i=0;i<lis.length-1;i++) {
+    test=lis[i].querySelector("div");
+  }
+return test;
+}
